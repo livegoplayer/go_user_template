601279e1e1d02649ec6185e50adcba45e63f148e
bug <2532917800@qq.com>
2020-07-09 21:22:29 +0800

    用户操作相关接口

---
+++ controller/User.go
@@ -0,0 +1,177 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	myHelper "github.com/livegoplayer/go_helper"
+	"github.com/livegoplayer/go_user_rpc/user"
+
+	userpb "github.com/livegoplayer/go_user_rpc/user/grpc"
+)
+
+func RegisterHandler(c *gin.Context) {
+	registerRequest := &userpb.RegisterRequest{}
+	err := c.Bind(registerRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.Register(c, registerRequest)
+
+	myHelper.CheckError(err, "新建用户失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func LoginHandler(c *gin.Context) {
+	loginRequest := &userpb.LoginRequest{}
+	err := c.Bind(loginRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.Login(c, loginRequest)
+
+	myHelper.CheckError(err, "登录失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func AddUserHandler(c *gin.Context) {
+	addUserRequest := &userpb.AddUserRequest{}
+	err := c.Bind(addUserRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.AddUser(c, addUserRequest)
+
+	myHelper.CheckError(err, "添加用户失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func DelUserHandler(c *gin.Context) {
+	delUserRequest := &userpb.DelUserRequest{}
+	err := c.Bind(delUserRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.DelUser(c, delUserRequest)
+
+	myHelper.CheckError(err, "删除用户失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func CheckUserStatusHandler(c *gin.Context) {
+	checkUserStatusRequest := &userpb.CheckUserStatusRequest{}
+	err := c.Bind(checkUserStatusRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.CheckUserStatus(c, checkUserStatusRequest)
+
+	myHelper.CheckError(err, "检查用户登录状态失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func CheckUserAuthorityHandler(c *gin.Context) {
+	checkUserAuthorityRequest := &userpb.CheckUserAuthorityRequest{}
+	err := c.Bind(checkUserAuthorityRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.CheckUserAuthority(c, checkUserAuthorityRequest)
+
+	myHelper.CheckError(err, "检查用户登录状态失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func GetUserAuthorityListHandler(c *gin.Context) {
+	getUserAuthorityListRequest := &userpb.GetUserAuthorityListRequest{}
+	err := c.Bind(getUserAuthorityListRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.GetUserAuthorityList(c, getUserAuthorityListRequest)
+
+	myHelper.CheckError(err, "获取用户权限列表失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func GetAuthorityListHandler(c *gin.Context) {
+	getAuthorityListRequest := &userpb.GetAuthorityListRequest{}
+	err := c.Bind(getAuthorityListRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.GetAuthorityList(c, getAuthorityListRequest)
+
+	myHelper.CheckError(err, "获取权限列表失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func AddUserRoleHandler(c *gin.Context) {
+	addUserRoleRequest := &userpb.AddUserRoleRequest{}
+	err := c.Bind(addUserRoleRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.AddUserRole(c, addUserRoleRequest)
+
+	myHelper.CheckError(err, "获取用户角色列表失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func DelUserRoleHandler(c *gin.Context) {
+	delUserRoleRequest := &userpb.DelUserRoleRequest{}
+	err := c.Bind(delUserRoleRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.DelUserRole(c, delUserRoleRequest)
+
+	myHelper.CheckError(err, "删除用户角色失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func GetRoleListHandler(c *gin.Context) {
+	getRoleListRequest := &userpb.GetRoleListRequest{}
+	err := c.Bind(getRoleListRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.GetRoleList(c, getRoleListRequest)
+
+	myHelper.CheckError(err, "获取角色列表失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
+
+func GetUserRoleListHandler(c *gin.Context) {
+	getUserRoleListRequest := &userpb.GetUserRoleListRequest{}
+	err := c.Bind(getUserRoleListRequest)
+
+	userClient := user.GetUserClient()
+	res, err := userClient.GetUserRoleList(c, getUserRoleListRequest)
+
+	myHelper.CheckError(err, "获取角色列表失败")
+
+	data := res.GetData()
+
+	myHelper.SuccessResp(c, "ok", data)
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/livegoplayer/go_user
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/livegoplayer/go_helper v0.0.0-20200629061256-7c48ccb9f7bc
+	github.com/livegoplayer/go_user_rpc v0.0.0-20200709033048-77747c90f51e
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/tebeka/strftime v0.1.4 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
