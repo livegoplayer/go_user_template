9b422d85d0c9da8d11587b96643480c6125f8086
bug <2532917800@qq.com>
2020-07-19 12:19:34 +0800

    更换access log

--- controller/test.go
+++ controller/test.go
@@ -1,39 +1,20 @@
 package controller
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	myHelper "github.com/livegoplayer/go_helper"
+	myLogger "github.com/livegoplayer/go_logger"
 )
 
 var redisClient *redis.Client
 var prefix = ""
 
 func MyTestHandler(c *gin.Context) {
+	myLogger.Info("test")
 	myHelper.SuccessResp(c, "ok")
 }
 
 func GetRedisClient() *redis.Client {
 	return redisClient
-}
-
-func init() {
-	// 根据redis配置初始化一个客户端
-	redisClient = redis.NewClient(&redis.Options{
-		Addr:     "139.224.132.234:6379", // redis地址
-		Password: "myredis",              // redis密码，没有则留空
-		DB:       0,                      // 默认数据库，默认是0
-	})
-
-	prefix = "fs_redis_"
-}
-
-func GetRedisKey(key string) *redis.StringCmd {
-	return redisClient.Get(prefix + key)
-}
-
-func SetRedisKey(key string, value []byte, expire time.Duration) *redis.StatusCmd {
-	return redisClient.Set(prefix+key, value, expire)
 }
